8bcd00d8893d2e0179c22116b28e7034a6797d0f
Martin Görner <martin.gorner@gmail.com>
2019-01-23 23:10:56 -0800

    TPUs can now use Tensorflow 1.12 on ML Engine

--- tensorflow-planespotting/cloudrun_yolo.bash
+++ tensorflow-planespotting/cloudrun_yolo.bash
@@ -41,7 +41,7 @@ gcloud ml-engine jobs submit training airplane_tpu$N \
     --region ${REGION} \
     --module-name trainer_yolo.main \
     --package-path trainer_yolo \
-    --runtime-version 1.9 \
+    --runtime-version 1.12 \
     -- \
     --tiledata "${TILEDATA}" \
     --hp-lr2 1000 \
